d3af67131821a494a6d5fc457a594babd62851fc
M Usama <80664846+M-Usama23@users.noreply.github.com>
2021-03-17 13:59:59 +0500

    add day function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
+import React from 'react';
+import Message from './message'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Message />
       </header>
     </div>
   );
 }
 
-export default App;
+
---
+++ src/message.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import './message.css'
+
+
+
+export default function Message(){
+    let [isDay, setDay] = useState(true)
+    return(
+        <div className={isDay ? 'box' : 'eve' }>
+        <h1>This is {isDay? 'Morning' : 'Evening'}</h1>
+        <button onClick={()=>{
+            setDay(!isDay)
+        }}>Change Time</button>
+    </div>
+    )
+}
